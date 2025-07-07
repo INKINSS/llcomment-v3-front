cf1da5005ec98f90e0b2013af7720de71ef6395c
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-07 15:38:25 -0600

    feat: crea el banner de la pagina principal

---
+++ src/components/banners/Welcome.tsx
@@ -0,0 +1,20 @@
+import { MainButton } from '../commons/buttons/MainButton';
+import { SecondButton } from '../commons/buttons/SecondButton';
+
+export const Welcome = () => {
+	return (
+		<section className="flex flex-col items-center justify-center prose w-full md:w-4/6 lg:w-3/6">
+			<h1 className="text-center font-semibold text-3xl sm:text-4xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-t from-slate-100 to-indigo-500">
+				comparte tu conocimiento con el mundo
+			</h1>
+			<p className="text-textSecondary text-center text-lg lg:text-2xl prose">
+				un sitio dedicado a las personas apasionadas por compartir su
+				conocimiento con el mundo
+			</p>
+			<div className="flex mt-5 gap-3">
+				<MainButton />
+				<SecondButton />
+			</div>
+		</section>
+	);
+};
