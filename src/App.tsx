1e476badf4429c145a5e7a369bcb4d77cc25c36e
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-15 15:11:31 -0600

    feat: :rocket: añade componente de proteccion de rutas y lo enruta en archivo principal
    posiblemente se cambie por zustand

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { MainLayout } from './layouts/MainLayout';
 import { WelcomePage } from './pages/WelcomePage';
+import { ProtectedRoute } from './components/protectedRoute/ProtectedRoute';
+import { DashboardPage } from './pages/DashboardPage';
+import { LoginPage } from './pages/LoginPage';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<MainLayout />}>
+				<Route path="/login" element={<LoginPage />} />
 					<Route index element={<WelcomePage />} />
+					<Route path="/dashboard" element={<ProtectedRoute />}>
+						<Route index element={<DashboardPage />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
