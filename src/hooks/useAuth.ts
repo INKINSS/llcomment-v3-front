74f7e4c0bc57ac2fadcda8eca3e32569dc69122f
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-15 15:10:34 -0600

    feat: añade hook para verificar si un usuario esta loggeado

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { api } from '../components/api/axios/axios';
+
+type User = {
+    id: string;
+    username: string;
+    email: string;
+};
+
+export const useAuth = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await api.get('/api/public/dashboard',{ withCredentials: true});
+                setUser(response.data);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchUser();
+    }, []);
+
+    return { user, loading };
+};
